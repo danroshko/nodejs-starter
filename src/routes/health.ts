1cabd77e6fe255dbbe71a5a99094db38cfd4a486
Danil Roshko <danroshko@gmail.com>
2018-07-28 09:57:33 +0500

    some additional tests

--- src/routes/health.ts
+++ src/routes/health.ts
@@ -18,4 +18,11 @@ router.get('/mongo', async ctx => {
   ctx.body = { result: 'MongoDB works' };
 });
 
+router.post('/body', async ctx => {
+  ctx.assert(typeof ctx.request.body.n === 'number', 400, 'Expecting number');
+
+  ctx.status = 201;
+  ctx.body = { result: 'Body parser works' };
+});
+
 export default router;
--- tests/health.test.ts
+++ tests/health.test.ts
@@ -23,4 +23,44 @@ describe('health routes', () => {
     expect(r.statusCode).toEqual(200);
     expect(r.body).toEqual({ result: 'MongoDB works' });
   });
+
+  test('post route', async () => {
+    const r = await got.post(BASE + 'body', { json: true, body: { n: 42 } });
+
+    expect(r.statusCode).toEqual(201);
+    expect(r.body).toEqual({ result: 'Body parser works' });
+  });
+});
+
+describe('error status codes', () => {
+  test('404', async () => {
+    expect.assertions(1);
+
+    try {
+      await got.get(BASE + 'asdfgh');
+    } catch (error) {
+      expect(error.response.statusCode).toEqual(404);
+    }
+  });
+
+  test('405', async () => {
+    expect.assertions(1);
+
+    try {
+      await got.post(BASE + 'app', { json: true, body: { text: 'hello' } });
+    } catch (error) {
+      expect(error.response.statusCode).toEqual(405);
+    }
+  });
+
+  test('400', async () => {
+    expect.assertions(2);
+
+    try {
+      await got.post(BASE + 'body', { json: true, body: { n: '42' } });
+    } catch (error) {
+      expect(error.response.statusCode).toEqual(400);
+      expect(error.response.body).toEqual('Expecting number');
+    }
+  });
 });
